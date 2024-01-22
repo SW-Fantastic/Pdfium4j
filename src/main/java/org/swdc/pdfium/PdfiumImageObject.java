62c1a6956cb5dec040129d86287d176351ca7ae2
SW-Fantastic <396146982@qq.com>
2024-01-22 18:38:40 +0800

    add new edit functions

--- src/main/java/org/swdc/pdfium/PdfiumImageObject.java
+++ src/main/java/org/swdc/pdfium/PdfiumImageObject.java
@@ -2,8 +2,12 @@ package org.swdc.pdfium;
 
 import org.swdc.pdfium.internal.PDFPageObjectImpl;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 
 public class PdfiumImageObject extends PdfiumPageObject {
@@ -12,18 +16,33 @@ public class PdfiumImageObject extends PdfiumPageObject {
         super(page, pointer, index);
     }
 
+    public boolean loadImage(InputStream inputStream) throws IOException {
+        checkState();
+        BufferedImage image = ImageIO.read(inputStream);
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        ImageIO.write(image,"jpg" , os);
+        byte[] data = os.toByteArray();
+        return PDFPageObjectImpl
+                .loadObjectJpegData(this.getPointer(), data);
+    }
+
     public boolean loadImageFile(File file) throws IOException {
         checkState();
         if (!file.exists()) {
             return false;
         }
         String name = file.getName().toLowerCase();
+        byte[] data = null;
         if (name.endsWith("jpg") || name.endsWith("jpeg")) {
-            byte[] data = Files.readAllBytes(file.toPath());
-            return PDFPageObjectImpl
-                    .loadObjectJpegData(this.getPointer(), data);
+            data = Files.readAllBytes(file.toPath());
+        } else {
+            BufferedImage image = ImageIO.read(file);
+            ByteArrayOutputStream os = new ByteArrayOutputStream();
+            ImageIO.write(image,"jpg" , os);
+            data = os.toByteArray();
         }
-        return false;
+        return PDFPageObjectImpl
+                .loadObjectJpegData(this.getPointer(), data);
     }
 
 
